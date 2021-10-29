e8310ca0b320be572509ef00354003da7707b14c
Adam Harpur <adam@adamharpur.com>
2021-10-29 09:26:23 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="conatainer">
+    <Eye />
+  </div>
+</template>
+
+<script lang="ts" >
+import Eye from "./Eye.vue";
+export default {
+  components: {
+    Eye,
+  },
+};
+</script>
+
+<style>
+.conatainer {
+  background-color: black;
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+body {
+  margin: 0;
+  height: 100vh;
+}
+</style>
